c48c4e677544147e143e4911e1b3245bdd26a0ab
DESKTOP-J4GQ28I\Mohit <mohit@ucreate.co.in>
2019-05-07 13:30:52 +0530

    update test case of sum of two number, should select all column from table

--- model/usertest.js
+++ model/usertest.js
@@ -1,3 +1,17 @@
-module.exports = function () {
+const helloWord = function () {
     return "hello";
+}
+
+const sum = (value1, value2) => {
+    return value1 + value2;
+};
+
+
+const getSqlQuery = (table, columns = []) => {
+    const selectColumns = columns.length > 0 ? '' : '*';
+    return `select ${selectColumns} from ${table}`;
+}
+
+module.exports = {
+    helloWord, sum, getSqlQuery
 }
--- test/demoTest.js
+++ test/demoTest.js
@@ -1,9 +1,24 @@
 
 const assert = require('chai').assert;
-const test = require('../model/usertest')
+const {sum,helloWord,getSqlQuery} = require('../model/usertest')
 
 describe('helloTest', () =>{
     it('App should return hello', ()=> {
-        assert.equal(test(), 'hello');
+        assert.equal(helloWord(), 'hello');
     });
-});
+});       
+
+
+describe('Sum value', function() {
+    it('should sum value', function() {
+        assert.equal(6, sum(2, 4));
+    })
+ })
+
+describe('Sql Query Builder: Select', () => {
+    it('should select all columns from specific table if columns not mentioned.', () => {
+        assert.equal('select * from users', getSqlQuery('users'));
+    })
+})
+
+
